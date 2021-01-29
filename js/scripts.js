8cad5f5d463d41747c86c0f6eeedc8c58dba833c
Andy Lawson <alawson89@gmail.com>
2021-01-29 09:32:59 -0800

    add 'size' business logic, change size from checkbox to radio buttons, css styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,33 @@ function Pizza(size, toppings, delivery) {
   this.delivery = delivery;
 }
 
+let order = function (pizza) {
+  let size = pizza.size;
+  let toppings = pizza.toppings;
+  let delivery = pizza.delivery;
+  let orderPrice = 10;
+
+  if (size === "small") {
+    orderPrice = orderPrice;
+  }
+  else if (size === "medium") {
+    orderPrice = orderPrice + 3;
+  } else if (size === "large") {
+    orderPrice = orderPrice + 5;
+  } else if (size === "colossal") {
+    orderPrice = orderPrice + 7;
+  } else {
+    orderPrice = orderPrice;
+  }
+  return orderPrice;
+}
 
 
 //User Interface Logic
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
-    const inputtedSize = $("input:checkbox[name=size]:checked").val();
+    const inputtedSize = $("input:radio[name=size]:checked").val();
     const inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
     const inputtedDelivery = $("input:checkbox[name=delivery]:checked").val();
     let newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedDelivery);
