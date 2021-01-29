c9b1795e240856ef265c7a9ecbbf04648082b621
Andy Lawson <alawson89@gmail.com>
2021-01-29 09:01:32 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-function Pizza(size, toppings) {
+function Pizza(size, toppings, delivery) {
   this.size = size;
   this.toppings = toppings;
+  this.delivery = delivery;
 }
+
+
+
+//User Interface Logic
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const inputtedSize = $("input:checkbox[name=size]:checked").val();
+    const inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
+    const inputtedDelivery = $("input:checkbox[name=delivery]:checked").val();
+    let newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedDelivery);
+    let orderPrice = order(newPizza);
+    $("#total").show().text("Total: " + orderPrice);
+  })
+});
