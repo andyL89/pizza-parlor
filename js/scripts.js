3c08055684fe18fb1ba9b228e2f7b2c6d05a7c7d
Andy Lawson <alawson89@gmail.com>
2021-01-29 10:32:25 -0800

    store toppings values in an array

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,16 @@ let order = function (pizza) {
 
   if (size === "small") {
     orderPrice = orderPrice + 10;
-  }
-  else if (size === "medium") {
+  } else if (size === "medium") {
     orderPrice = orderPrice + 13;
   } else if (size === "large") {
     orderPrice = orderPrice + 15;
   } else if (size === "colossal") {
     orderPrice = orderPrice + 17;
-  } else {
-    orderPrice = orderPrice;
+  } else if (delivery === "delivery")
+    orderPrice = orderPrice + 3;
+  else {
+    orderPrice = orderPrice + toppings;
   }
   return orderPrice;
 }
@@ -32,14 +33,15 @@ $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
     const inputtedSize = $("input:radio[name=size]:checked").val();
+    let chosenToppings = [];
     $("input:checkbox[name=toppings]:checked").each(function () {
       const inputtedToppings = $(this).val();
-
+      chosenToppings.push(inputtedToppings);
+    });
       const inputtedDelivery = $("input:radio[name=delivery]:checked").val();
-      let newPizza = new Pizza(inputtedSize, inputtedToppings, inputtedDelivery);
+      let newPizza = new Pizza(inputtedSize, chosenToppings, inputtedDelivery);
       let orderPrice = order(newPizza);
       $("#total").show().text("Total: " + orderPrice);
       console.log(newPizza);
     })
-  });
 });
